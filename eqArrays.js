1709865dc4204def5e821033345366772d3dab5f
Brad Williams <bradley_williams2003@hotmail.com>
2022-11-15 15:13:41 -0400

    Fixed function, now fully working

--- eqArrays.js
+++ eqArrays.js
@@ -7,7 +7,14 @@ const eqArrays = function (array1, array2) {
   let isTrue = false;
 
   for (let i = 0; i < array1.length; i++) {
-    array1[i] === array2[0 + i] ? isTrue = true : isTrue = false;
+    if (array1[i] === array2[0 + i]) {
+      isTrue = true
+    } else {
+      isTrue = false;
+    }
+    if (isTrue === false) {
+      return false;
+    }
   }
   return isTrue;
 };
@@ -15,5 +22,5 @@ const eqArrays = function (array1, array2) {
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
 assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays([1, 2, 9], [3, 2, 1]), false);
 assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
