0744112663ae4f496e67bfc3d00cdf81f8c77016
Brad Williams <bradley_williams2003@hotmail.com>
2022-11-21 08:47:24 -0400

    Removed old test code from files

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,16 +1,3 @@
-// const middle = require('../middle');
-// const eqArrays = require('../eqArrays');
-// const assertArraysEqual = require('../assertArraysEqual');
-
-// //TEST CODE
-// //PASSING TESTS
-// assertArraysEqual(middle([1]), []);
-// assertArraysEqual(middle([1, 2]), []);
-// assertArraysEqual(middle([1, 2, 3]), [2]);
-// assertArraysEqual(middle([1, 2, 4, 5, 6]), [4]);
-// assertArraysEqual(middle([1, 2, 4, 5, 6, 7]), [4, 5]);
-// assertArraysEqual(middle([1, 2, 4, 3, 2, 5]), [4, 3]);
-
 const assert = require('chai').assert;
 const middle = require('../middle');
 
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,3 @@
-// const assertEqual = require('../assertEqual');
-// const tail = require('../tail');
-
-// //TEST CODE
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// const test = tail([]);
-// const test1 = tail(["Hello"]);
-// assertEqual(result.length, 2); // ensure we get back two elements
-// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-// assertEqual(test.length, 0);
-// assertEqual(test1.length, 0);
-
 const assert = require('chai').assert;
 const tail = require('../tail');
 
